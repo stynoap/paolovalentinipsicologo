13648083232f07249c0c7846bd996bc75366e115
stynoap <giulia.pinna47@tiscali.it>
2024-07-25 15:41:39 +0200

    cambio background

--- src/Components/SubTitles.js
+++ src/Components/SubTitles.js
@@ -1,7 +1,7 @@
 function SubTitles({ text }) {
   return (
     <>
-      <h2 className="text-xl font-semibold py-3 bg-sky-50 my-5 w-fit px-4 rounded-full text-sky-700">{text}</h2>
+      <h2 className="text-xl font-semibold py-3 bg-sky-100 my-5 w-fit px-4 rounded-full text-sky-700">{text}</h2>
     </>
   );
 }
