f871b1446a56aefc33eabe30183ddba8b1a04aa2
stynoap <giulia.pinna47@tiscali.it>
2024-07-26 18:14:41 +0200

    prova numero 657575

--- src/Components/FormContatto.js
+++ src/Components/FormContatto.js
@@ -9,10 +9,10 @@ function FormContatto() {
       >
         
       
-        <div class="text-3xl mb-6 text-center ">Contattami</div>
+        {/* <div class="text-3xl mb-6 text-center ">Contattami</div>
 
         <div class="grid grid-cols-2 gap-4 max-w-xl m-auto">
-          <p class="col-span-2 lg:col-span-1">
+          <p class="col-span-2 lg:col-span-1"> */}
             <input
               type="text"
               class="border-solid border-sky-600 border-2 p-3 md:text-xl w-full"
@@ -20,9 +20,9 @@ function FormContatto() {
               name="Nome"
               required
             />
-          </p>
+          {/* </p> */}
 
-          <p class="col-span-2 lg:col-span-1">
+          {/* <p class="col-span-2 lg:col-span-1"> */}
             <input
               type="text"
               class="border-solid border-sky-600 border-2 p-3 md:text-xl w-full"
@@ -30,9 +30,9 @@ function FormContatto() {
               name="indirizzo-email"
               required
             />
-          </p>
+          {/* </p> */}
 
-          <p class="col-span-2">
+          {/* <p class="col-span-2"> */}
             <textarea
               cols="30"
               rows="8"
@@ -41,17 +41,17 @@ function FormContatto() {
               name="testo-del-messaggio"
               required
             ></textarea>
-          </p>
+          {/* </p> */}
 
-          <div class="col-span-2 text-right">
+          {/* <div class="col-span-2 text-right"> */}
             <button
               type="submit"
               class="py-3 px-6 bg-sky-800 text-white font-bold w-full sm:w-32"
             >
               Invia
             </button>
-          </div>
-        </div>
+          {/* </div>
+        </div> */}
       </form>
     </>
   );
