c7c17a097f567c3942668bca29227b7a52e35ef3
stynoap <giulia.pinna47@tiscali.it>
2024-07-26 19:47:41 +0200

    alcune modifiche

--- src/Components/FormContatto.js
+++ src/Components/FormContatto.js
@@ -6,7 +6,9 @@ function FormContatto() {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
-    });
+    })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => console.error("Error submitting form:", error));
   };
   return (
     <>
