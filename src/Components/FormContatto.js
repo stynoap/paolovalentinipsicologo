a156c74971841d30dbeb9d3c49e5ee29671bbd2e
stynoap <giulia.pinna47@tiscali.it>
2024-07-26 18:42:48 +0200

    form contatto test

--- src/Components/FormContatto.js
+++ src/Components/FormContatto.js
@@ -57,31 +57,32 @@ function FormContatto() {
           {/* </div>
         </div> 
       </form> */}
-      <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
-        <input
-          type="hidden"
-          name="subject"
-          value="Sales inquiry from mysitename.netlify.app"
-        />
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+       <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Nome: <input type="text" name="name" required />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email: <input type="email" name="email" required />
+        </label>
+      </p>
+      <p>
+        <label>
+          Messaggio: <textarea name="message" required></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Invia</button>
+      </p>
+    </form>
     </>
   );
 }
