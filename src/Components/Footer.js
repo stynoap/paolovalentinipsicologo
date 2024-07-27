7677cc7170716901e10fb341641a4a1c7e999e0d
Raimon <60296186+Neune0@users.noreply.github.com>
2024-07-27 17:16:42 +0200

    tolta doppia nav e inserito logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,7 @@
+import logo from "./../logo_paolo_valentini.png"
+
 function Footer(){
+
     return <>
     
 
@@ -7,7 +10,7 @@ function Footer(){
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
+                  <img src={logo} class="h-8 me-3" alt="Paolo Valentini Logo" />
                   <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Paolo Valentini</span>
               </a>
           </div>
--- src/Pages/Approccio.js
+++ src/Pages/Approccio.js
@@ -6,16 +6,7 @@ import Article from "../Components/Article";
 function Approccio() {
   return (
     <>
-      <div
-        className=" bg-cover max-h-fit"
-        style={{
-          backgroundImage: `url(${headerbg})`,
-        }}
-      >
-        <div className=" bg-cover bg-bottom pb-20  ">
-          <Navbar></Navbar>
-        </div>
-      </div>
+      
       <main className="">
         <Article></Article>
       </main>
