f1c3512115748ade3d1441d860e17dbb650a007e
stynoap <giulia.pinna47@tiscali.it>
2024-07-27 18:57:46 +0200

    cambio tariffe

--- src/Components/Sezioni/Tariffe.js
+++ src/Components/Sezioni/Tariffe.js
@@ -7,9 +7,8 @@ function Tariffe() {
       <div id="tariffe" className="mt-24 mb-8 ">
       <TitleSection text="Tariffario" />
       <div className="flex flex-col lg:flex-row min-w-60 items-center lg:flex-wrap lg:gap-7 justify-center">
-        <PriceCard servizio="Colloquio psicologico" prezzo="50" />
-        <PriceCard servizio="Terapia di coppia" prezzo="60" />
-        <PriceCard servizio="Orientamento" prezzo="40" />
+        <PriceCard servizio="Colloquio psicologico individuale" prezzo="40" />
+        <PriceCard servizio="Colloquio psicologico di coppia" prezzo="60" />
       </div>
       </div>
      
