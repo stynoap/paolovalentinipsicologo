51a5091123e726a9665b88ae4cfcf9da0f1826a7
stynoap <giulia.pinna47@tiscali.it>
2024-07-31 15:59:41 +0200

    price card bg

--- src/Components/PriceCard.js
+++ src/Components/PriceCard.js
@@ -1,7 +1,7 @@
 function PriceCard({servizio, prezzo}){
     return(
         <>
-        <div className="rounded-2xl mt-20 flex flex-col px-24 bg-sky-50 leading-7 bg-opacity-35 max-w-96 tracking-wider h-60 gap-8 uppercase items-center justify-center shadow-md shadow-slate-300">
+        <div className="rounded-2xl mt-20 flex flex-col px-24 bg-white leading-7 bg-opacity-40 max-w-96 tracking-wider h-60 gap-8 uppercase items-center justify-center shadow-md shadow-slate-300">
           <p className="text-2xl font-semibold text-slate-800">{servizio}</p>
           <p className="text-5xl font-semibold text-slate-800">{prezzo}€</p>
         </div>
