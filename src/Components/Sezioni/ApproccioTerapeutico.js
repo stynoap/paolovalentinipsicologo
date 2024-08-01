8f4d635b7b7c2a2200ad27caf4f2dedfd77b5dc5
stynoap <giulia.pinna47@tiscali.it>
2024-08-01 21:32:47 +0200

    modifiche in homepage

--- src/Components/Sezioni/ApproccioTerapeutico.js
+++ src/Components/Sezioni/ApproccioTerapeutico.js
@@ -3,7 +3,7 @@ import TitleSection from "../TitleSection";
 import SubTitles from "../SubTitles";
 import SecondaryButton from "../SecondaryButton";
 import { useNavigate } from "react-router-dom";
-import piantina from "../../icone/piantina.jpeg";
+// import piantina from "../../icone/piantina.jpeg";
 import congiunzioni from "../../icone/congiunzioni.jpeg";
 import ghianda from "../../icone/ghianda.jpg";
 
@@ -22,35 +22,6 @@ function ApproccioTerapeutico() {
         <div className="lg:w-8/12 ">
           <div className="mt-14 leading-9 gap-4 flex flex-col  text-slate-900 tracking-wide font-medium mx-10 text-left items-center w-fit">
             <div>
-              <SubTitles text="La psicologia analitica" />
-              <div
-                className="h-72 lg:w-8/12 w-full bg-no-repeat bg-center lg:float-left mr-10 rounded-lg"
-                style={{
-                  backgroundImage: `url(${piantina})`,
-                }}
-              ></div>
-              <p className="text-xs leading-7 italic mb-9 lg:mt-0 mt-4">
-                {" "}
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged.
-              </p>
-
-              <p className="clear-both ">
-                {" "}
-                La psicologia analitica è un metodo di indagine del prodondo
-                elaborato dall'analista Carl Gustav Jung. L'approccio si
-                distingue per un interesse al mondo interiore del paziente, il
-                cui disturbo psichico non è più una malattia, il sintomo non
-                richiede più una spigazione di causa-effetto ed entrambi
-                esprimono la richiesta di una domanda di significato. La
-                guarigione o la cura non sono più gli scopi principali,
-                l'obiettivo è l'avvio di un processo che mira ad individuare
-                l'orizzonte simbolico e archetipico del disturbo.{" "}
-              </p>
 
               <SubTitles
                 text="La psicologia
@@ -73,14 +44,27 @@ function ApproccioTerapeutico() {
               </p>
 
               <p className="clear-both">
-                La psicologia archetipica, denominazione che risale a James
-                Hillman è una psicologia che volutamente si collega con le arti,
-                la cultura e la storia della società, le quali traggono
-                anch'esse origine dall'immaginazione. Secondo la definizione
-                tradizionale, gli archetipi sono le forme primarie che governano
-                la psiche. Essi non possono però essere contenuti unicamente
-                dalla psiche dato che si manifestano anche nelle modalità
-                fisica, sociale, linguistica, estetica e sprituale.
+                Il pricincio cardine della psicologia Archetipica risulta
+                nell'attribuire l'origine dei processi psichici alla psiche,
+                compresa la malattia. James Hillman è il fondatore della
+                psicologia archetipica. Si distacca dalle prospettive della
+                psicologia moderna, rielabora la tradizione immaginativa di C.G.
+                Jung e rimette al centro del suo campo di studi un'idea antica e
+                universale:l'idea di anima. L'anima, in greco psyche è soffio
+                vitale e essenza energetica della vita. L'anima intesa però non
+                come sostanza ma come prospettiva e visuale sulle cose. La
+                psiche attraverso la fantasia svolge un attività creativa
+                costante in cui le immagini e le rappresentazioni immaginative
+                sono i suoi dati basilari, materia prima e prodotto finito allo
+                stesso tempo, esse costituiscono le vie d'accesso alla
+                conoscenza dell'anima. Alla radice dell'anima ci sono gli
+                archetipi i modelli primari e profondi del funzionamento
+                psichico, pattern of beahviour che influenzano il nostro modo di
+                vedere noi stessi e il mondo e di agire in questo rapporto. La
+                loro primarietà ci consente di concepire gli eventi che accadono
+                in una prospettiva nella quale possiamo collegare quanto avviene
+                dentro di noi a quanto avviene a tutti gli individui nel corso
+                di ogni tempo e luogo.
               </p>
               <SubTitles text="La teoria della ghianda" />
               <div
@@ -115,24 +99,6 @@ function ApproccioTerapeutico() {
                 proponendo invece una prospettiva che mette in risalto
                 l'importanza di scoprire e vivere in armonia con il proprio
                 destino personale e autentico.
-              </p>
-
-              <SubTitles
-                text="La psicologia del
-              profondo per Hillman. "
-              />
-
-              <p>
-                Per Hillman tutto ha un significato archetipico e tutto è aperto
-                alla penetrazione psicologica. La fantasia del profondo
-                incoraggia a guardare il mondo con altri occhi, a leggere ogni
-                evento in cerca di ‟qualcosa di più profondo", a ‟cercare
-                dentro" anziché a ricercare, per estrarre un significato che
-                vada oltre quanto appare evidente. La fantasia delle profondità
-                nascoste infonde anima al mondo e favorisce un immaginare che
-                penetri sempre più profondamente nelle cose. Il profondo -
-                anziché un luogo nel senso letterale o fisico ,è una metafora
-                primaria necessaria al pensare psicologico.
               </p>
             </div>
             <SecondaryButton
